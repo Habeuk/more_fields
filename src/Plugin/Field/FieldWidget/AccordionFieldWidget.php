e0f592f91213a558813b31d3161232dbe4a7861e
vystii <ngahabanda@gmail.com>
2023-04-06 17:26:36 +0100

    add accordion fields

---
+++ src/Plugin/Field/FieldWidget/AccordionFieldWidget.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Drupal\more_fields\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Field\Annotation\FieldWidget;
+use Drupal\Core\Annotation\Translation;
+
+/**
+ * Plugin implementation of the 'more_fields_accordion_field' widget.
+ *
+ * @FieldWidget(
+ *   id = "more_fields_accordion_field_widget",
+ *   module = "more_fields",
+ *   label = @Translation("Accordion field widget type"),
+ *   field_types = {
+ *     "more_fields_accordion_field"
+ *   }
+ * )
+ */
+class AccordionFieldWidget extends WidgetBase
+{
+
+    /**
+     *
+     * {@inheritdoc}
+     */
+    public static function defaultSettings()
+    {
+        return [
+            'label_1' => "Icon",
+            'label_2' => "Title",
+            'label_3' => "Description"
+        ] + parent::defaultSettings();
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     */
+    public function settingsForm(array $form, FormStateInterface $form_state)
+    {
+        $elements = [];
+        $elements['label_1'] = [
+            '#type' => 'textfield',
+            '#title' => t('label 1'),
+            '#default_value' => $this->getSetting('label_1')
+        ];
+        $elements['label_2'] = [
+            '#type' => 'textfield',
+            '#title' => t('label 2'),
+            '#default_value' => $this->getSetting('label_2')
+        ];
+        $elements['label_3'] = [
+            '#type' => 'textfield',
+            '#title' => t('label 3'),
+            '#default_value' => $this->getSetting('label_3')
+        ];
+        return $elements;
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     */
+    public function settingsSummary()
+    {
+        $summary = [];
+        return $summary;
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     */
+    public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state)
+    {
+        $elements = [];
+        if (!empty($element['#title_display']))
+            unset($element['#title_display']);
+        $elements['title'] = [
+            '#title' => $this->t('Title'),
+            '#type' => 'textfield',
+            '#default_value' => isset($items[$delta]->title) ? $items[$delta]->title : NULL
+        ] + $element;
+        //
+        $elements['icon'] = [
+            '#title' => $this->t('Icon'),
+            '#type' => 'text_format',
+            '#format' => isset($items[$delta]->format) ? $items[$delta]->format : 'basic_html',
+            '#default_value' => isset($items[$delta]->icon) ? $items[$delta]->icon : NULL
+        ] + $element;
+        //
+        $elements['description'] = [
+            '#title' => $this->t('Description'),
+            '#type' => 'text_format',
+            '#default_value' => isset($items[$delta]->description) ? $items[$delta]->description : NULL
+        ] + $element;
+        return $elements;
+    }
+    function massageFormValues($values, $form, $form_state)
+    {
+        $vals = parent::massageFormValues($values, $form, $form_state);
+        dump($values);
+        foreach ($vals as &$val) {
+            if (isset($val['text']['format'])) {
+                $val['format'] = $val['text']['format'];
+            }
+            if (isset($val['icon']['format'])) {
+                $val['format'] = $val['icon']['format'];
+            }
+            $val['description'] = $val['description']['value'];
+            $val['icon'] = $val['icon']['value'];
+        }
+        return $vals;
+    }
+}
