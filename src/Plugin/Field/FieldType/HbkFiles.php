85ade0e82426e07b4fea05500ac9b7a24bca6e9e
kouwa stephane <kksasteph888@gmail.com>
2023-12-18 08:03:02 +0100

    update name and add require video

--- src/Plugin/Field/FieldType/HbkFiles.php
+++ src/Plugin/Field/FieldType/HbkFiles.php
@@ -15,7 +15,7 @@ use Drupal\Core\Field\FieldStorageDefinitionInterface;
  *
  * @FieldType(
  *   id = "more_fields_hbk_file",
- *   label = @Translation("HBK File"),
+ *   label = @Translation("Galleries Files (videos and images) "),
  *   description = @Translation("This field stores the ID of a file(video or image) as an integer value."),
  *   default_widget = "file_generic",
  *   default_formatter = "more_fields_hbk_file_formatter",
@@ -25,33 +25,39 @@ use Drupal\Core\Field\FieldStorageDefinitionInterface;
  * )
  */
 class HbkFiles extends FileItem {
-    /**
-     * {@inheritdoc}
-     */
-    public static function defaultFieldSettings() {
-        $parentFieldSettings = parent::defaultFieldSettings();
-        if (isset($parentFieldSettings["file_extensions"])) {
-            unset($parentFieldSettings["file_extensions"]);
-        }
-        return [
-            'file_extensions' =>  'mp4,ogv,webm,png,gif,jpg,jpeg,webp',
-            'image_extensions' => 'png, gif, jpg, jpeg, webp',
-            'video_extensions' => 'mp4, ogv, webm',
-        ] + $parentFieldSettings;
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public static function defaultFieldSettings() {
+    $parentFieldSettings = parent::defaultFieldSettings();
+    if (isset($parentFieldSettings["file_extensions"])) {
+      unset($parentFieldSettings["file_extensions"]);
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    // public function fieldSettingsForm(array $form, FormStateInterface $form_state) {
-    //     $settings = $this->getSettings();
-    //     $element = parent::fieldSettingsForm($form, $form_state);
-    //     $element["image_extensions"] = $element["file_extensions"];
-    //     $element["video_extensions"] = $element["file_extensions"];
-    //     $element["image_extensions"]["#default_value"] = $settings["image_extensions"] ?? "";
-    //     $element["video_extensions"]["#default_value"] = $settings["video_extensions"] ?? "";
-    //     unset($element["file_extensions"]);
-    //     // dump([$element, $settings]);
-    //     return $element;
-    // }
+    return [
+      'file_extensions' => 'mp4,ogv,webm,png,gif,jpg,jpeg,webp',
+      'image_extensions' => 'png, gif, jpg, jpeg, webp',
+      'video_extensions' => 'mp4, ogv, webm'
+    ] + $parentFieldSettings;
+  }
+  
+/**
+ *
+ * {@inheritdoc}
+ */
+  // public function fieldSettingsForm(array $form, FormStateInterface
+  // $form_state) {
+  // $settings = $this->getSettings();
+  // $element = parent::fieldSettingsForm($form, $form_state);
+  // $element["image_extensions"] = $element["file_extensions"];
+  // $element["video_extensions"] = $element["file_extensions"];
+  // $element["image_extensions"]["#default_value"] =
+  // $settings["image_extensions"] ?? "";
+  // $element["video_extensions"]["#default_value"] =
+  // $settings["video_extensions"] ?? "";
+  // unset($element["file_extensions"]);
+  // // dump([$element, $settings]);
+  // return $element;
+  // }
 }
