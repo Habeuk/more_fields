220f8a1f839ace017403c4b795daace42fea0abb
vystii <ngahabanda@gmail.com>
2023-12-18 12:08:40 +0100

    modification de la logique dans le formatteur hbkFilesFormatter

--- src/Plugin/Field/FieldFormatter/HbkFilesFormatter.php
+++ src/Plugin/Field/FieldFormatter/HbkFilesFormatter.php
@@ -14,6 +14,7 @@ use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\File\FileUrlGeneratorInterface;
 use Drupal\file\Entity\File;
+use Drupal\Core\Cache\Cache;
 use Drupal\file\Plugin\Field\FieldType\FileFieldItemList;
 use Google\Service\HangoutsChat\Resource\Dms;
 
@@ -29,10 +30,11 @@ use Google\Service\HangoutsChat\Resource\Dms;
  * )
  */
 class HbkFilesFormatter extends GenericFileFormatter implements ContainerFactoryPluginInterface {
-  protected $providerManager;
+  protected $imageStyleStorage;
+  protected $fileUrlGenerator;
   protected $videoFormatter;
   protected $imageFormatter;
-  
+
   /**
    * Constructs a new instance of the plugin.
    *
@@ -57,16 +59,34 @@ class HbkFilesFormatter extends GenericFileFormatter implements ContainerFactory
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
     $this->videoFormatter = new VideoPlayerListFormatter($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $current_user);
     $this->imageFormatter = new ImageFormatter($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $current_user, $image_style_storage, $file_url_generator);
+    $this->imageStyleStorage = $image_style_storage;
+    if (!$file_url_generator) {
+      @trigger_error('Calling ImageFormatter::__construct() without the $file_url_generator argument is deprecated in drupal:9.3.0 and the $file_url_generator argument will be required in drupal:10.0.0. See https://www.drupal.org/node/2940031', E_USER_DEPRECATED);
+      $file_url_generator = \Drupal::service('file_url_generator');
+    }
+    $this->fileUrlGenerator = $file_url_generator;
   }
-  
+
   /**
    *
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static($plugin_id, $plugin_definition, $configuration['field_definition'], $configuration['settings'], $configuration['label'], $configuration['view_mode'], $configuration['third_party_settings'], $container->get('video.provider_manager'), $container->get('current_user'), $container->get('entity_type.manager')->getStorage('image_style'), $container->get('file_url_generator'));
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('video.provider_manager'),
+      $container->get('current_user'),
+      $container->get('entity_type.manager')->getStorage('image_style'),
+      $container->get('file_url_generator')
+    );
   }
-  
+
   /**
    *
    * {@inheritdoc}
@@ -82,7 +102,7 @@ class HbkFilesFormatter extends GenericFileFormatter implements ContainerFactory
     $default["image_settings"]["field_extension"] = "png, gif, jpg, jpeg, webp";
     return $default;
   }
-  
+
   /**
    *
    * {@inheritdoc}
@@ -111,7 +131,7 @@ class HbkFilesFormatter extends GenericFileFormatter implements ContainerFactory
       "preload",
       'field_extension'
     ];
-    
+
     $temp_form['video_settings'] = [
       '#type' => 'details',
       '#title' => $this->t('Video Settings'),
@@ -124,10 +144,10 @@ class HbkFilesFormatter extends GenericFileFormatter implements ContainerFactory
       '#tree' => TRUE,
       '#open' => FALSE
     ];
-    
+
     $video_settings_form = $this->videoFormatter->settingsForm($form, $form_state);
     $image_settings_form = $this->imageFormatter->settingsForm($form, $form_state);
-    
+
     $field_extension = [
       "#title" => $this->t("field type extension"),
       "#type" => "textfield",
@@ -135,10 +155,10 @@ class HbkFilesFormatter extends GenericFileFormatter implements ContainerFactory
     ];
     $temp_form["video_settings"]["field_extension"] = $field_extension;
     $temp_form["image_settings"]["field_extension"] = $field_extension;
-    
+
     $temp_form['image_settings'] = array_merge($temp_form['image_settings'], $image_settings_form);
     $temp_form['video_settings'] = array_merge($temp_form['video_settings'], $video_settings_form);
-    
+
     $settings_form = [
       // utilile pour mettre à jour le style
       'layoutgenentitystyles_view' => [
@@ -152,117 +172,131 @@ class HbkFilesFormatter extends GenericFileFormatter implements ContainerFactory
     foreach ($video_settings_fields as $value) {
       $temp_form["video_settings"][$value]["#default_value"] = $video_settings[$value];
     }
-    
+
     // update default value for image
     foreach ($image_settings_fields as $value) {
       $temp_form["image_settings"][$value]["#default_value"] = $image_settings[$value];
     }
-    
+
     // dump($temp_form);
     $settings_form = array_merge($settings_form, $temp_form);
     return $settings_form + parent::settingsForm($form, $form_state);
   }
-  
+
   /**
    *
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $files = $this->getEntitiesToView($items, $langcode);
-    dump($files);
-    /**
-     *
-     * @var \Drupal\more_fields\Plugin\Field\FieldType\HbkFiles $item
-     */
+    $elements = [];
+    $entity = $items->getEntity();
     $image_settings = $this->getSetting("image_settings");
     $video_settings = $this->getSetting("video_settings");
-    $images = [];
-    $images_render_array = [];
-    $videos = [];
-    $videos_render_array = [];
-    $otherFiles = [];
-    $other_files_array = [];
-    $order_array = [];
-    foreach ($items as $delta => $item) {
-      /**
-       *
-       * @var File $file
-       */
-      $file = File::load($item->target_id);
+
+    $files = $this->getEntitiesToView($items, $langcode);
+
+    $url = NULL;
+    $image_link_setting = $image_settings["image_link"] ?? "file";
+    // Check if the formatter involves a link.
+    if ($image_link_setting == 'content') {
+      if (!$entity->isNew()) {
+        $url = $entity->toUrl();
+      }
+    } elseif ($image_link_setting == 'file') {
+      $link_file = TRUE;
+    }
+
+    $image_style_setting = $this->getSetting("image_settings")['image_style'];
+    $image_loading_settings = $image_settings['image_loading'];
+    // Collect cache tags to be added for each item in the field.
+    $base_cache_tags = [];
+    if (!empty($image_style_setting)) {
+      $image_style = $this->imageStyleStorage->load($image_style_setting);
+      $base_cache_tags = $image_style->getCacheTags();
+    }
+
+    /**
+     * @var File $file
+     */
+    foreach ($files as $delta => $file) {
+      //get file extension
       $file_extension = pathinfo($file->getFileUri(), PATHINFO_EXTENSION);
       if (strpos($image_settings["field_extension"], $file_extension) !== false) {
-        //
-        $images[$delta] = $item;
-        $order_array[] = 1;
-      }
-      elseif (strpos($video_settings["field_extension"], $file_extension) !== false) {
-        $videos[$delta] = $item;
-        $order_array[] = 2;
-      }
-      else {
-        $otherFiles[$delta] = $item;
-        $order_array[] = 0;
+        //Gestion des images
+        $this->viewImageElement($file, $elements, $url, $image_style_setting, $base_cache_tags, $image_loading_settings, $delta, isset($link_file) ? $link_file : NULL);
+      } elseif (strpos($video_settings["field_extension"], $file_extension) !== false) {
+        //Gestion des videos
+        $this->viewVideoElement([$file], $elements, $delta);
+      } else {
+        //Autres types de fichiers
+        $this->viewParentElement($file, $elemnets, $delta);
       }
     }
-    
-    // Handling images
-    
-    $this->videoFormatter->setSettings($video_settings);
-    $this->imageFormatter->setSettings($image_settings);
-    // $items contient les images et les videos, donc tu ne peux pas faire le
-    // rendu ainsi ?
-    $video_render_array = $this->videoFormatter->viewElements($items, $langcode);
-    $image_render_array = $this->imageFormatter->viewElements($items, $langcode);
-    $other_file_render_array = parent::viewElements($items, $langcode);
-    
-    $elements = [];
-    foreach ($order_array as $key => $value) {
-      switch ($value) {
-        case 1:
-          $elements[$key] = $image_render_array[$key];
-          break;
-        case 2:
-          $elements[$key] = $video_render_array;
-          $videos_render_array[$key][0]["#items"] = [];
-          $videos_render_array[$key][0]["#items"][] = $video_render_array[0]["#items"][$key] ?? "";
-          break;
-        case 0:
-          $elements[$key] = $other_file_render_array[$key];
-          break;
-        default:
-          break;
-      }
-    }
+
     return [
       "#theme" => "more_field_file_image_video",
       "items" => $elements
     ];
   }
-  
-  protected function viewVideoElement(array $files, &$elements) {
+
+  protected function viewVideoElement(array $files, &$elements, $delta) {
     $video_items = [];
-    foreach ($files as $delta => $file) {
+    foreach ($files as  $file) {
       $video_items[] = \Drupal::service('file_url_generator')->generateAbsoluteString($file->getFileUri());
     }
-    $elements[] = [
+    $elements[$delta] = [
       '#theme' => 'video_player_formatter',
       '#items' => $video_items,
       '#player_attributes' => $this->getSetting('video_settings')
     ];
   }
-  
-  public function buildRenderArray($formatter, $order_array, $context, $settings_array, $items, $langcode) {
-    $result = [];
-    $formatter->setSettings($settings_array);
-    $render_array = $formatter->viewElements($items, $langcode);
-    // dd($render_array);
-    foreach ($order_array as $key => $value) {
-      if ($value == $context) {
-        $result[$key] = $render_array[$key];
-      }
+
+  /**
+   * @param File $file
+   */
+  protected function viewImageElement($file, &$elements, $url, $image_style_setting, $base_cache_tags, $image_loading_settings, $delta, $link_file = NULL) {
+    if (isset($link_file)) {
+      $image_uri = $file->getFileUri();
+      $url = $this->fileUrlGenerator->generate($image_uri);
     }
-    return $result;
+    $cache_tags = Cache::mergeTags($base_cache_tags, $file->getCacheTags());
+
+    // Extract field item attributes for the theme function, and unset them
+    // from the $item so that the field template does not re-render them.
+    $item = $file->_referringItem;
+    $item_attributes = $item->_attributes;
+    unset($item->_attributes);
+
+    $item_attributes['loading'] = $image_loading_settings['attribute'];
+
+    $elements[$delta] = [
+      '#theme' => 'image_formatter',
+      '#item' => $item,
+      '#item_attributes' => $item_attributes,
+      '#image_style' => $image_style_setting,
+      '#url' => $url,
+      '#cache' => [
+        'tags' => $cache_tags,
+      ],
+    ];
   }
-  
-  // Implement other methods as needed.
+
+  protected function viewParentElement($file, &$elements, $delta) {
+    $item = $file->_referringItem;
+    $elements[$delta] = [
+      '#theme' => 'file_link',
+      '#file' => $file,
+      '#cache' => [
+        'tags' => $file->getCacheTags(),
+      ],
+    ];
+    // Pass field item attributes to the theme function.
+    if (isset($item->_attributes)) {
+      $elements[$delta] += ['#attributes' => []];
+      $elements[$delta]['#attributes'] += $item->_attributes;
+      // Unset field item attributes since they have been included in the
+      // formatter output and should not be rendered in the field template.
+      unset($item->_attributes);
+    }
+  }
 }
