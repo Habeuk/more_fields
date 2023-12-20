d79af66814ef95bfbe916430099ac4f67b7911e2
vystii <ngahabanda@gmail.com>
2023-12-20 10:14:39 +0100

    ajout de la config slidePerViews et mise en page des slides videos

--- wbu-atomique-theme/src/js/fields/field-files.js
+++ wbu-atomique-theme/src/js/fields/field-files.js
@@ -1,2 +1,3 @@
 import '@stephane888/wbu-atomique/js/swiper/swiper-drupal.js';
+import "@stephane888/wbu-atomique/scss/organisme/nebuleuse/scss/commerce-gallery-custom.scss";
 import "@stephane888/wbu-atomique/scss/organisme/nebuleuse/scss/commerce-gallery-v3.scss";
