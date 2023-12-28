3003f4bbb8fe03d31f10a6d6f99fe13352e28c7e
vystii <ngahabanda@gmail.com>
2023-12-28 11:42:52 +0100

    maj service more_fields_video.video_converter

--- modules/video/src/Services/MoreFieldsVideoConverter.php
+++ modules/video/src/Services/MoreFieldsVideoConverter.php
@@ -15,14 +15,15 @@ class MoreFieldsVideoConverter {
     use StringTranslationTrait;
 
     protected $thumb_extension = '.png';
-
+    protected $thumb_mime = 'image/png';
     /**
      * create the thumb file for a video in a given format (the default format is png)
      * @param int $video_id
      * Id of the seed Drupal\file\Entity\File (video) to be converted 
      * @param int $frame_second
      * the second where the frame will be captured
-     * @return string $path
+     * @return File|boolean
+     * the not saved file that have been generated or false
      */
     public function createThumbFile($video_id, $frame_seconde = 1) {
         $file = File::load($video_id);
@@ -39,9 +40,16 @@ class MoreFieldsVideoConverter {
         $file_system = \Drupal::service('file_system');
         $ffm_video = $ffmpeg->open($file_system->realpath($file_uri));
         try {
-            $temp = $ffm_video->frame(TimeCode::fromSeconds($frame_seconde))
+            $ffm_video->frame(TimeCode::fromSeconds($frame_seconde))
                 ->save($file_system->realpath($thumb_path));
-            return $thumb_path;
+            /**
+             * @var File $thumb_file
+             */
+            $thumb_file = File::create();
+            $thumb_file->setFileUri($thumb_path);
+            $thumb_file->setFilename(pathinfo($thumb_path, PATHINFO_FILENAME));
+            $thumb_file->setMimeType($this->thumb_mime);
+            return $thumb_file;
         } catch (\Throwable $th) {
             return FALSE;
         }
@@ -49,8 +57,12 @@ class MoreFieldsVideoConverter {
 
     /**
      * define in the extension of the thumb when it will be genereted
+     * at the same time it define the thumb mime
+     * @param string $extension 
+     * extension of the thumb. ex: png, jpeg, webp(not supported yet)
      */
     public function setThumbExtension($extension) {
-        $this->thumb_extension = $extension;
+        $this->thumb_extension = "." . $extension;
+        $this->thumb_mime = "image/" . $extension;
     }
 }
