bcdcf6809faebf8bd293893776f33bd5c3627227
kouwa stephane <kksasteph888@gmail.com>
2024-11-01 12:03:06 +0100

    Maj

--- src/Plugin/Field/FieldFormatter/ValueNiveauProgressBar.php
+++ src/Plugin/Field/FieldFormatter/ValueNiveauProgressBar.php
@@ -134,5 +134,4 @@ class ValueNiveauProgressBar extends ValueNiveauFormatterType {
     ];
     return $container;
   }
-  
 }
---
+++ src/Plugin/Field/FieldFormatter/VideoWithConverter.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\more_fields\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\video\Plugin\Field\FieldFormatter\VideoPlayerListFormatter;
+use Drupal\Core\Template\Attribute;
+
+/**
+ * Plugin implementation of the 'experience_formatter_type' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "more_fields_video_with_converter",
+ *   label = @Translation("Display videos with type"),
+ *   field_types = {
+ *     "video"
+ *   }
+ * )
+ */
+class VideoWithConverter extends VideoPlayerListFormatter {
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    $files = $this->getEntitiesToView($items, $langcode);
+    
+    // Early opt-out if the field is empty.
+    if (empty($files)) {
+      return $elements;
+    }
+    
+    // Collect cache tags to be added for each item in the field.
+    $video_items = [];
+    foreach ($files as $file) {
+      /**
+       *
+       * @var \Drupal\file\Entity\File $file
+       */
+      $video_items[] = [
+        'src' => \Drupal::service('file_url_generator')->generateAbsoluteString($file->getFileUri()),
+        'filemime' => $file->getMimeType()
+      ];
+    }
+    $elements[] = [
+      '#theme' => 'more_fields_video_player_with_type_formatter',
+      '#items' => $video_items,
+      '#video_attributes' => new Attribute($this->getSettings())
+    ];
+    return $elements;
+  }
+}
