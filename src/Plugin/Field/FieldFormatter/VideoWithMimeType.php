feb9cd77e2d8be881ff9809e8353a2826260fd11
kouwa stephane <kksasteph888@gmail.com>
2024-12-11 09:55:42 +0100

    Maj

--- src/Plugin/Field/FieldFormatter/VideoWithMimeType.php
+++ src/Plugin/Field/FieldFormatter/VideoWithMimeType.php
@@ -6,6 +6,7 @@ use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\video\Plugin\Field\FieldFormatter\VideoPlayerListFormatter;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Template\Attribute;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Plugin implementation of the 'experience_formatter_type' formatter.
@@ -19,6 +20,35 @@ use Drupal\Core\Template\Attribute;
  * )
  */
 class VideoWithMimeType extends VideoPlayerListFormatter {
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      "layoutgenentitystyles_view" => "more_fields/more_fields_video_with_converter",
+      "control_js" => false
+    ] + parent::defaultSettings();
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $elements = parent::settingsForm($form, $form_state);
+    $elements['layoutgenentitystyles_view'] = [
+      '#type' => 'hidden',
+      "#value" => $this->getSetting("layoutgenentitystyles_view")
+    ];
+    $elements['control_js'] = [
+      '#title' => t('Controls with JS'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('control_js')
+    ];
+    return $elements;
+  }
   
   /**
    *
@@ -45,14 +75,32 @@ class VideoWithMimeType extends VideoPlayerListFormatter {
         'filemime' => $file->getMimeType()
       ];
     }
+    $valids_attributes = [
+      "width" => "width",
+      "height" => "height",
+      "autoplay" => "autoplay",
+      "loop" => "loop",
+      "muted" => "muted",
+      "preload" => "preload",
+      "controls" => "controls"
+    ];
+    $settings = $this->getSettings();
+    $videos_settings = [
+      'class' => [
+        'videos_control',
+        $this->getSetting("control_js") ? "with_js" : ""
+      ]
+    ];
+    foreach ($valids_attributes as $value) {
+      if (!empty($settings[$value]))
+        $videos_settings[$value] = $settings[$value];
+    }
     
-    $settings = array_filter($this->getSettings(), function ($item) {
-      return $item ? true : false;
-    });
     $elements[] = [
       '#theme' => 'more_fields_video_player_with_type_formatter',
       '#items' => $video_items,
-      '#video_attributes' => new Attribute($settings)
+      '#video_attributes' => new Attribute($videos_settings),
+      '#all_settings' => $settings
     ];
     return $elements;
   }
---
+++ wbu-atomique-theme/src/js/fields/more_fields_video_with_converter.js
@@ -0,0 +1,3 @@
+import "@stephane888/wbu-atomique/scss/drupal/modules/more_fields/field/more_fields_video_with_converter/more_fields_video_with_converter.js";
+import "@stephane888/wbu-atomique/scss/drupal/modules/more_fields/field/more_fields_video_with_converter/more_fields_video_with_converter.scss";
+
