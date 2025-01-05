96d8a37167e6114f4b425944ce931ecac37db1ff
kouwa stephane <kksasteph888@gmail.com>
2025-01-05 08:34:41 +0100

    Maj

--- src/Plugin/Block/TitreDeLaPageEncoursBlock.php
+++ src/Plugin/Block/TitreDeLaPageEncoursBlock.php
@@ -15,7 +15,9 @@ use Drupal\Core\Form\FormStateInterface;
  * )
  */
 class TitreDeLaPageEncoursBlock extends BlockBase {
-
+  protected $request;
+  protected $route_match;
+  
   /**
    *
    * {@inheritdoc}
@@ -23,10 +25,11 @@ class TitreDeLaPageEncoursBlock extends BlockBase {
   public function defaultConfiguration() {
     return [
       'suffix_title' => '',
-      'tag' => 'h1'
+      'tag' => 'h1',
+      'custom_class' => ''
     ];
   }
-
+  
   /**
    *
    * {@inheritdoc}
@@ -42,9 +45,14 @@ class TitreDeLaPageEncoursBlock extends BlockBase {
       '#title' => $this->t('tag to use'),
       '#default_value' => $this->configuration['tag']
     ];
+    $form['custom_class'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('custom class'),
+      '#default_value' => $this->configuration['custom_class']
+    ];
     return $form;
   }
-
+  
   /**
    *
    * {@inheritdoc}
@@ -52,43 +60,58 @@ class TitreDeLaPageEncoursBlock extends BlockBase {
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->configuration['suffix_title'] = $form_state->getValue('suffix_title');
     $this->configuration['tag'] = $form_state->getValue('tag');
+    $this->configuration['custom_class'] = $form_state->getValue('custom_class');
   }
-
+  
   /**
    *
    * {@inheritdoc}
    */
   public function build() {
-    $request = \Drupal::request();
-    $route_match = \Drupal::routeMatch();
-    /**
-     *
-     * @var \Drupal\Core\Controller\TitleResolver $titleResolver
-     */
-    $titleResolver = \Drupal::service('title_resolver');
-    $title = $titleResolver->getTitle($request, $route_match->getRouteObject());
-    if (!empty($this->configuration['suffix_title']))
-      $title = $title . ' ' . $this->configuration['suffix_title'];
-
+    $title = $this->buildTitre();
     if (!empty($this->configuration['tag'])) {
       $build['title'] = [
         '#type' => 'html_tag',
         '#tag' => $this->configuration['tag'],
+        '#attributes' => [
+          'class' => [
+            $this->configuration['custom_class']
+          ]
+        ],
         $this->viewValue($title)
       ];
     }
     else
       $build = $this->viewValue($title);
-
+    
     return $build;
   }
-
+  
+  /**
+   * Contruit le titre.
+   *
+   * @return string|NULL
+   */
+  protected function buildTitre() {
+    $this->request = \Drupal::request();
+    $this->route_match = \Drupal::routeMatch();
+    /**
+     *
+     * @var \Drupal\Core\Controller\TitleResolver $titleResolver
+     */
+    $titleResolver = \Drupal::service('title_resolver');
+    $title = $titleResolver->getTitle($this->request, $this->route_match->getRouteObject());
+    if (!empty($this->configuration['suffix_title']))
+      $title = $title . ' ' . $this->configuration['suffix_title'];
+    return $title;
+  }
+  
   /**
    * Generate the output appropriate for one field item.
    *
    * @param \Drupal\Core\Field\FieldItemInterface $item
    *        One field item.
-   *
+   *        
    * @return array The textual output generated as a render array.
    */
   protected function viewValue($value) {
@@ -102,5 +125,4 @@ class TitreDeLaPageEncoursBlock extends BlockBase {
       ]
     ];
   }
-
 }
